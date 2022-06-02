99ca08dac6b9b89e51955cb9f1f20d021dbc14ac
PoFoq <pofoq@mail.ru>
2022-06-02 20:36:15 +0300

    examples

---
+++ ASP.NET MVC Core/2. Углубление в мультипоточность/lesson02.md
@@ -0,0 +1,3 @@
+presentation
+https://docs.google.com/presentation/d/1-0qlwjg9pSWPuHM0Xn8D3g2g_IWvw3CbcWaGvtDALK0/edit#slide=id.g12f7d2aeb57_0_47
+
---
+++ ASP.NET MVC Core/Examples/Lesson01/01_MultyThreadApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+namespace Interview.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Thread thread = new Thread(new ThreadStart(SomeMethod));
+            thread.Start();
+            Thread.Sleep(9000);
+            Console.WriteLine("Stop");
+            SomeMethod(2);
+        }
+
+        private static void SomeMethod()
+        {
+            SomeMethod(1);
+        }
+
+        private static void SomeMethod(int threadNum)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine($"{threadNum} - {i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
